3892fc78a637288b9f824154540321b9c57babd8
febriantikayas <febriantikayas@gmail.com>
2026-08-06 13:19:23 +0700

    Refactor file paths for model and dataset
    Updated file paths to be dynamic instead of hardcoded.

--- src/CNN feature extraction/extraction.py
+++ src/CNN feature extraction/extraction.py
@@ -3,9 +3,9 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-model_path = r"D:\SKRIPSI\hasil latih\model_cnn\best_model_baru.h5"
-train_dir  = r"D:\SKRIPSI\dataset_train"
-val_dir    = r"D:\SKRIPSI\dataset_vald"
+model_path = 
+train_dir  = 
+val_dir    = 
 
 model = tf.keras.models.load_model(model_path)
 
@@ -22,8 +22,7 @@ train_gen = datagen.flow_from_directory(
     train_dir,
     target_size=(224, 224),
     batch_size=32,
-    class_mode='sparse',
-    shuffle=False   # PENTING! supaya urutan tetap
+    class_mode='sparse'
 )
 
 print(train_gen.class_indices)
@@ -50,12 +49,11 @@ y_val   = val_gen.classes
 train_files = train_gen.filenames
 val_files   = val_gen.filenames
 
-# Ambil hanya nama file tanpa folder class
 train_files = [os.path.basename(f) for f in train_files]
 val_files   = [os.path.basename(f) for f in val_files]
 
 np.savez(
-    r"D:\SKRIPSI\hasil validasi\model_cnn\fitur_train_val.npz",
+    r"",
     X_train=X_train,
     y_train=y_train,
     train_files=train_files,
