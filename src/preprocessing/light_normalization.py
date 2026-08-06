733a4c494c9d2c724ed835876664939c81ca60da
febriantikayas <febriantikayas@gmail.com>
2026-08-06 12:40:00 +0700

    Refactor light normalization script for flexibility
    Removed hardcoded input and output folder paths, and gamma value.

--- src/preprocessing/light_normalization.py
+++ src/preprocessing/light_normalization.py
@@ -2,14 +2,13 @@ import cv2
 import numpy as np
 import os
 
-input_folder = r"D:/SKRIPSI/dataset_test/pelat baik"
-output_folder = r"D:/SKRIPSI/dataset_test/3 pelat baik/"
+input_folder =
+output_folder = 
 
-# Buat folder output jika belum ada
 os.makedirs(output_folder, exist_ok=True)
 
-gamma = 2.0
-gamma_exp = 2.75 / gamma   # sesuai program kamu
+gamma = 
+gamma_exp = 2.75 / gamma   
 
 for filename in os.listdir(input_folder):
     if filename.lower().endswith(('.jpg')):
@@ -21,13 +20,11 @@ for filename in os.listdir(input_folder):
             print(f"Gagal membaca: {filename}")
             continue
 
-        # Gamma correction
         gamma_correction = np.array(
             255 * (img / 255) ** gamma_exp,
             dtype='uint8'
         )
 
-        # Simpan ke folder output
         output_path = os.path.join(output_folder, filename)
         cv2.imwrite(output_path, gamma_correction)
 
